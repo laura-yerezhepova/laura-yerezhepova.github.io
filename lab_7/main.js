6306d333ed71f1b3ed66baf289ec638059c25d68
Laura <laura.yerezhepova@nu.edu.kz>
2023-03-12 00:17:12 +0600

    practice

---
+++ lab_7/main.js
@@ -0,0 +1,48 @@
+console.log(document);
+
+let h1 = document.createElement("h1");
+h1.innerText = 'Lab7 Assignment'
+h1.style.color = "blue";
+document.body.appendChild(h1);
+
+var elem = document.createElement("hr");
+elem.setAttribute("width", "auto");
+document.body.appendChild(elem);
+
+
+let h2 = document.createElement("h2");
+h2.innerText = 'Task'
+h2.style.color = "red";
+document.body.appendChild(h2);
+
+let p1 = document.createElement("p1")
+p1.innerText = 'In this task you have to reproduce this HTML page as is using only JavaScript. The task will be evaluated on the basis of the following DOM manipulation techniques:';
+p1.style.color = 'black';
+document.body.appendChild(p1);
+
+
+var elem = document.createElement("hr");
+elem.setAttribute("width", "auto");
+document.body.appendChild(elem);
+
+
+let h3 = document.createElement("h3");
+h3.innerText = 'Submission'
+h3.style.color = "red";
+document.body.appendChild(h3);
+
+let p2 = document.createElement('p2');
+p2.innerText = 'To submit your work, follow these instructions:'
+p2.style.color = 'black';
+document.body.appendChild(p2);
+
+
+var elem = document.createElement("hr");
+elem.setAttribute("width", "auto");
+document.body.appendChild(elem);
+
+
+console.log(document);
+
+
+
