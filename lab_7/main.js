c7b30aeb5493c6e1fc99a99d49e919d11da67ab2
Laura <laura.yerezhepova@nu.edu.kz>
2023-03-12 14:53:07 +0600

    practice

--- lab_7/main.js
+++ lab_7/main.js
@@ -15,10 +15,41 @@ h2.innerText = 'Task'
 h2.style.color = "red";
 document.body.appendChild(h2);
 
-let p1 = document.createElement("p1")
+let p1 = document.createElement("p");
 p1.innerText = 'In this task you have to reproduce this HTML page as is using only JavaScript. The task will be evaluated on the basis of the following DOM manipulation techniques:';
 p1.style.color = 'black';
 document.body.appendChild(p1);
+
+
+let li1 = document.createElement('li')
+li1.textContent = 'find elements in the DOM (5 points);';
+li1.style.color = 'green';
+document.body.appendChild(li1);
+
+let li2 = document.createElement('li')
+li2.textContent = 'create/add/remove elements (5 points);';
+li2.style.color = 'purple';
+document.body.appendChild(li2);
+
+let li3 = document.createElement('li')
+li3.textContent = 'change content of the elements (5 points);';
+li3.style.color = 'green';
+document.body.appendChild(li3);
+
+let li4 = document.createElement('li')
+li4.textContent = 'change styles of the elements (5 points);';
+li4.style.color = 'purple';
+document.body.appendChild(li4);
+
+let li5 = document.createElement('li')
+li5.textContent = 'change attributes of the elements (5 points);';
+li5.style.color = 'green';
+document.body.appendChild(li5);
+
+let li6 = document.createElement('li')
+li6.textContent = 'change classes of the elements (5 points).';
+li6.style.color = 'purple';
+document.body.appendChild(li6);
 
 
 var elem = document.createElement("hr");
@@ -36,6 +67,40 @@ p2.innerText = 'To submit your work, follow these instructions:'
 p2.style.color = 'black';
 document.body.appendChild(p2);
 
+let li7 = document.createElement('li')
+li7.textContent = 'Create a new repository on Github, named lab7 (1 point).';
+li7.style.color = 'green';
+document.body.appendChild(li7);
+
+let li8 = document.createElement('li')
+li8.textContent = 'Clone this repository to your local machine and work inside it.';
+li8.style.color = 'purple';
+document.body.appendChild(li8);
+
+let li9 = document.createElement('li')
+li9.textContent = 'Create a new HTML file, called index.html, which has only one <h1> tag with "Hello, World!" message (1 point).';
+li9.style.color = 'green';
+document.body.appendChild(li9);
+
+let li10 = document.createElement('li')
+li10.textContent = 'Create a new JavaScript file, called main.js, which must contain your program (assignment) described above (1 point).';
+li10.style.color = 'purple';
+document.body.appendChild(li10);
+
+let li11 = document.createElement('li')
+li11.textContent = 'Link this main.js file to your index.html file (Note: place your script at the end of the body section).';
+li11.style.color = 'green';
+document.body.appendChild(li11);
+
+let li12 = document.createElement('li')
+li12.textContent = 'Write a JavaScript program in main.js to make your index.html look identical to this HTML file (5 points).';
+li12.style.color = 'purple';
+document.body.appendChild(li12);
+
+let li13 = document.createElement('li')
+li13.textContent = 'After you finish your work, submit it to the Github (2 points).';
+li13.style.color = 'green';
+document.body.appendChild(li13);
 
 var elem = document.createElement("hr");
 elem.setAttribute("width", "auto");
